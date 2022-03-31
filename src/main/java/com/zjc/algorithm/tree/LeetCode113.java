726fbd2d581e06bd677b23286767912a5adb751d
zoujianchao <data_zjc@163.com>
2022-03-31 09:29:55 +0800

    algorithm2022

---
+++ src/main/java/com/zjc/algorithm/tree/LeetCode113.java
@@ -0,0 +1,34 @@
+package com.zjc.algorithm.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author : zoujianchao
+ * @version : 1.0
+ * @date : 2022/3/31
+ * @description : 路径总和 II
+ */
+public class LeetCode113 {
+    private  List<List<Integer>> list = new ArrayList<>();
+    private List<Integer> res = new ArrayList<>();
+    
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        path(root, 0, targetSum);
+        return list;
+    }
+    
+    public void path(TreeNode root, int num, int sum) {
+        if (root == null) {
+            return;
+        }
+        num+=root.val;
+        res.add(root.val);
+        if (root.left == null && root.right == null && sum == num) {
+            list.add(new ArrayList<>(res));
+        }
+        path(root.left, num, sum);
+        path(root.right, num, sum);
+        res.remove(res.size() - 1);
+    }
+}
