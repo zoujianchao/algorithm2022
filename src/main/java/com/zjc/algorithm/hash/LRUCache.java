ce3b4a4c4f89506a225023886623becde751c111
zoujianchao <data_zjc@163.com>
2022-04-13 15:13:31 +0800

    algorithm2022

---
+++ src/main/java/com/zjc/algorithm/hash/LRUCache.java
@@ -0,0 +1,38 @@
+package com.zjc.algorithm.hash;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * @author : zoujianchao
+ * @version : 1.0
+ * @date : 2022/4/13
+ * @description : LRU
+ */
+public class LRUCache<K, V> extends LinkedHashMap {
+    private final int CACHE_SIZE;
+    
+    /**
+     * 传递进来最多能缓存多少数据
+     *
+     * @param cacheSize 缓存大小
+     */
+    public LRUCache(int cacheSize) {
+        //true 表示让 LinkedHashMap 按照顺序来进行排序, 最近访问的放在头部, 最老访问的放在尾部
+        super((int) (Math.ceil(cacheSize / 0.75) + 1), 0.75f, true);
+        CACHE_SIZE = cacheSize;
+    }
+    
+    
+    /**
+     * 钩子方法, 通过put新增键值对的时候, 若该方法返回true, 便移除该map中最老的键和值
+     *
+     * @param eldest
+     * @return
+     */
+    @Override
+    protected boolean removeEldestEntry(Map.Entry eldest) {
+        //当map中的数据量大于指定的缓存个数时候,就自动删除最老的数据
+        return size() > CACHE_SIZE;
+    }
+}
