efdd463b110380c2fbfa42695c9fe35406dd6eb1
zoujianchao <data_zjc@163.com>
2022-04-22 16:27:48 +0800

    algorithm2022s

--- src/main/java/com/zjc/algorithm/array/LeetCode209.java
+++ src/main/java/com/zjc/algorithm/array/LeetCode209.java
@@ -7,11 +7,30 @@ package com.zjc.algorithm.array;
  * @description : 长度最小的子数组
  */
 public class LeetCode209 {
-    public static int minSubArrayLen(int target, int[] nums) {
+    //    public static int minSubArrayLen(int target, int[] nums) {
+//        int left = 0;
+//        int sum = 0;
+//        int res = Integer.MAX_VALUE;
+//
+//        for (int right = 0; right < nums.length; right++) {
+//            sum += nums[right];
+//            while (sum >= target) {
+//                res = Math.min(res, right - left + 1);
+//                sum -= nums[left++];
+//            }
+//        }
+//        return res == Integer.MAX_VALUE ? 0 : res;
+//    }
+//
+//    public static void main(String[] args) {
+//        int[] nums = {2,3,1,2,4,3};
+//        minSubArrayLen(7, nums);
+//    }
+    public int minSubArrayLen(int target, int[] nums) {
         int left = 0;
         int sum = 0;
         int res = Integer.MAX_VALUE;
-        
+    
         for (int right = 0; right < nums.length; right++) {
             sum += nums[right];
             while (sum >= target) {
@@ -20,10 +39,5 @@ public class LeetCode209 {
             }
         }
         return res == Integer.MAX_VALUE ? 0 : res;
-    }
-    
-    public static void main(String[] args) {
-        int[] nums = {2,3,1,2,4,3};
-        minSubArrayLen(7, nums);
     }
 }
--- src/main/java/com/zjc/algorithm/array/LeetCode283.java
+++ src/main/java/com/zjc/algorithm/array/LeetCode283.java
@@ -7,14 +7,25 @@ package com.zjc.algorithm.array;
  * @description : 移动零
  */
 public class LeetCode283 {
+    //    public void moveZeroes(int[] nums) {
+//        int j = 0;
+//        for (int i = 0; i < nums.length ; i++) {
+//            if (nums[i] != 0) {
+//                nums[j++] = nums[i];
+//            }
+//        }
+//
+//        for (int i = j; i < nums.length; i++) {
+//            nums[i] = 0;
+//        }
+//    }
     public void moveZeroes(int[] nums) {
         int j = 0;
-        for (int i = 0; i < nums.length ; i++) {
+        for (int i = 0; i < nums.length; i++) {
             if (nums[i] != 0) {
                 nums[j++] = nums[i];
             }
         }
-    
         for (int i = j; i < nums.length; i++) {
             nums[i] = 0;
         }
