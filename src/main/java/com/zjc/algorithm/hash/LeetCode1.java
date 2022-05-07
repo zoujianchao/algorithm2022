148ae670778a8f49af49f71061fa9960ecb1018a
zoujianchao <data_zjc@163.com>
2022-05-07 23:47:52 +0800

    algorithm2022s

--- src/main/java/com/zjc/algorithm/hash/LeetCode1.java
+++ src/main/java/com/zjc/algorithm/hash/LeetCode1.java
@@ -10,11 +10,21 @@ import java.util.Map;
  * @description : 两数之和
  */
 public class LeetCode1 {
+    //    public int[] twoSum(int[] nums, int target) {
+//        Map<Integer, Integer> map = new HashMap<>();
+//        for (int i = 0; i < nums.length; i++) {
+//            if (map.containsKey(target - nums[i])) {
+//                return new int[] {map.get(target - nums[i]), i};
+//            }
+//            map.put(nums[i], i);
+//        }
+//        return new int[0];
+//    }
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             if (map.containsKey(target - nums[i])) {
-                return new int[] {map.get(target - nums[i]), i};
+                return new int[]{map.get(target - nums[i]), i};
             }
             map.put(nums[i], i);
         }
